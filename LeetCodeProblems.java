3f1baca2c96d150c47f965616efb58078d293c93
Bashmaistro <128954585+Bashmaistro@users.noreply.github.com>
2024-07-02 17:30:27 +0300

    443. String Compression

--- LeetCodeProblems.java
+++ LeetCodeProblems.java
@@ -145,6 +145,59 @@ var moveZeroes = function(nums) {
 };
 
 
+// 443. String Compression
+
+
+// Given an array of characters chars, compress it using the following algorithm:
+
+// Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+// If the group's length is 1, append the character to s.
+// Otherwise, append the character followed by the group's length.
+// The compressed string s should not be returned separately, but instead, be stored in the input 
+// character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+// After you are done modifying the input array, return the new length of the array.
+
+// You must write an algorithm that uses only constant extra space.
+
+ 
+
+function compress(chars) {
+    let writeIndex = 0;
+    let readIndex = 0;
+    const n = chars.length;
+
+    while (readIndex < n) {
+        const char = chars[readIndex];
+        let count = 0;
+
+        // Count the number of consecutive repeating characters
+        while (readIndex < n && chars[readIndex] === char) {
+            readIndex++;
+            count++;
+        }
+
+        // Write the character to the writeIndex
+        chars[writeIndex] = char;
+        writeIndex++;
+
+        // If the count is greater than 1, write the count as well
+        if (count > 1) {
+            const countStr = count.toString();
+            for (const digit of countStr) {
+                chars[writeIndex] = digit;
+                writeIndex++;
+            }
+        }
+    }
+
+    // Return the new length of the array
+    return writeIndex;
+}
+
+
+  
 
 
 
