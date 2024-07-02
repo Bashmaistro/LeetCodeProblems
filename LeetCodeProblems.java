1735c566694459a53c15d8c3a4a6f2e6042bd225
Bashmaistro <128954585+Bashmaistro@users.noreply.github.com>
2024-07-02 17:28:29 +0300

    Update and rename MergeSortedArray.java to LeetCodeProblems.java

--- LeetCodeProblems.java
+++ LeetCodeProblems.java
@@ -63,3 +63,70 @@ public class MergeSortedArray {
     }
 
 }
+
+
+
+// 392. Is Subsequence
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+// A subsequence of a string is a new string that is formed from the original string by deleting some 
+// (can be none) of the characters without disturbing the relative positions of the remaining characters. 
+// (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+// Example 1:
+
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+// Example 2:
+
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    
+    
+    let k=0;
+
+    for(i=0;i<t.length;i++){
+        if(t[i] == s[k]){
+           
+            k++;
+        }
+    }
+
+    if(k == s.length){
+        return true;
+    }else{
+        return false
+    }
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
