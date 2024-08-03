8a29e921858eaecec3ecc81c422754033c403b54
Bashmaistro <emirhanyildiz@ogr.eskisehir.edu.tr>
2024-08-03 20:16:20 +0300

    [649] Dota2 Senate

---
+++ 649.dota-2-senate.ts
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=649 lang=typescript
+ *
+ * [649] Dota2 Senate
+ */
+
+// @lc code=start
+function predictPartyVictory(senate: string): string {
+    const rQueue = []
+    const dQueue = []
+    const N = senate.length
+
+    for (let i = 0; i < N; i++) {
+        if (senate[i] === 'R') rQueue.push(i)
+        else dQueue.push(i)
+    }
+
+    while (rQueue.length && dQueue.length) {
+        const rVal = rQueue.shift()
+        const dVal = dQueue.shift()
+        if (rVal < dVal) {
+            rQueue.push(rVal + N)
+        } else {
+            dQueue.push(dVal + N)
+        }
+    }
+
+    return rQueue.length ? 'Radiant' : 'Dire'
+};
+// @lc code=end
+
